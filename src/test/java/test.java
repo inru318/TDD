2cb05ae087a09d80039b6988edc502e85e04d16d
王映儒 <113598015@cc.ntut.edu.tw>
2024-10-24 16:52:47 +0800

    v1 - createOneTask(fail)

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,2 +1,12 @@
+import java.util.Arrays;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
 public class test {
+    @Test
+    public void createOneTask() throws Exception {
+        Task createTask = new Task(1, 5, Arrays.asList(2, 3));
+        assertEquals(5,createTask.getTime());
+        assertEquals(Arrays.asList(2, 3),createTask.getDependencies());
+    }
 }
