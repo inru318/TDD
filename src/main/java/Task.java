932bcaaa14e513568462a3f032ec07474f352ce6
王映儒 <113598015@cc.ntut.edu.tw>
2024-10-24 17:29:08 +0800

    v16 - add expection handling in Task class & pass illegalTaskCase()

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -5,10 +5,13 @@ public class Task {
     private int time;
     private List<Integer> dependencies;
 
-    public Task(int id, int time, List<Integer> dependencies) {
+    public Task(int id, int time, List<Integer> dependencies) throws Exception{
         this.id = id;
         this.time = time;
         this.dependencies = dependencies;
+        if (hasCycle(this.id)) {
+            throw new Exception("Should be directed Acyclic Graph");
+        }
     }
 
     public int getTime() {
@@ -17,6 +20,17 @@ public class Task {
 
     public List<Integer> getDependencies() {
         return dependencies;
+    }
+
+    private boolean hasCycle(int taskId) {
+        if (dependencies != null) {
+            for (Integer dependency : dependencies) {
+                if (dependency <= taskId) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
 }
